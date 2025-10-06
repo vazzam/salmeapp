311aa8b9510c6a44927b6e6516d9c2f92dc945c9
vazzam <50787171+vazzam@users.noreply.github.com>
2025-10-05 18:34:29 -0600

    Update app_functions.py

--- app_functions.py
+++ app_functions.py
@@ -22,7 +22,26 @@ from openai import OpenAI
 import google.generativeai as genai
 from pymongo import MongoClient
 from dotenv import load_dotenv
-
+import random
+from datetime import date, datetime
+import streamlit as st
+from unidecode import unidecode
+from pymongo import MongoClient
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from openai import OpenAI
+import google.generativeai as genai
+import re
+import threading
+import io
+import wave
+from pydub import AudioSegment
+from streamlit_mic_recorder import mic_recorder
+import os
+from dotenv import load_dotenv
+import tempfile
+from pathlib import Path
+import time
 # ==================== CONFIGURACIÓN INICIAL ====================
 
 # Logging
@@ -420,5 +439,245 @@ def main():
                                 mime="text/plain"
                             )
 
-if __name__ == "__main__":
-    main()
+
+
+def calculate_age(born):
+    today = datetime.now()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+def clin_merge(scale):
+    if scale != '':
+        return f' {scale}, '
+    else:
+        return ''
+
+def radio_check(var):
+    if var != '':
+        return 'Yes'
+    else:
+        return ''
+
+def update_dict(dic,var):
+    dic.update({var:'Yes',})
+
+def id_gen():
+    now = datetime.now()
+    date_id = now.strftime('%d%m%y%H%M%S')
+    return int(date_id)
+# @st.cache_data
+def ensure_index(action, collection, index_name, index_key):
+    """
+    Ensure that a specified index exists on a collection. If the index does not
+    exist, create it using the specified index key.
+
+    :param action: Must be an str an lets us to 'delete' or 'create' an index.
+    :param collection: The MongoDB collection to ensure the index on.
+    :param index_name: The name of the index to check/create.
+    :param index_key: A dictionary of field names and sort orders to use as the index key.
+    """
+    if action == 'create':
+        # Check if the index exists
+        if index_name not in [idx['name'] for idx in collection.list_indexes()]:
+            # If the index does not exist, create it
+            collection.create_index(index_key, name=index_name)
+            print(f"Created index '{index_name}' on collection '{collection.name}'")
+        else:
+            print(f"Index '{index_name}' already exists on collection '{collection.name}'")
+    else:
+        collection.drop_index(index_name)
+        print(f'Index {index_name} has been deleted')
+# @st.cache_data
+def search_collection(collection, criteria, all_info = True):
+    """
+    Search a MongoDB collection for documents that match a set of criteria.
+
+    :param collection: The MongoDB collection to search.
+    :param criteria: A dictionary of search criteria.
+    :return: A list of all documents in the collection that match the criteria.
+    """
+    results = []
+    if all_info:
+        for document in collection.find(criteria):
+            results.append(document)
+        return results
+    else:
+        for document in collection.find(criteria,{'_id': 0, 'nombres':1,'primer apellido':1,'segundo apellido':1,'generales.nacimiento.fecha': 1}):
+            results.append(document)
+        return results
+    st.write(results)
+
+def unidecode_except(string):
+    # Replace all characters except for the ones in the exception list
+    exceptions = ['ñ','1','2','3','4','5','6','7','8','9','0',]
+    replaced_string = ''
+    for c in string:
+        if c in exceptions:
+            replaced_string += c
+        else:
+            replaced_string += unidecode(c)
+
+    return replaced_string
+
+def data_format(field, val):
+    """
+    :param field: Debe ser array
+    :param val: Debe ser array
+    """
+    for i in range(len(val)):
+        val[i]= unidecode_except(val[i])
+
+    # print(val)
+
+    temp_ar = {}
+    for i in range(len(field)):
+        temp_ar[field[i]] = {"$regex": val[i],"$options": "i"}
+        # print(f'{i}. {temp_ar}')
+    # print(temp_ar['nombres'])
+    return temp_ar
+
+def doc_field( database_name, collection_name, filter, projection):
+    # Set up a MongoDB client and database
+    db = database_name
+
+    # Set up a collection in the database
+    collection = db[collection_name]
+
+    # Find all matching documents with the specified fields
+    documents = collection.find(filter, projection)
+
+    # Loop through the documents and return the specified fields
+    results = []
+    for document in documents:
+        result = {}
+        for field in projection:
+            result[field] = document[field]
+        results.append(result)
+    return results
+
+def buscar_clientes(nombre, apellido_paterno, apellido_materno):
+    # crear una conexión con la base de datos
+    db = ['expedinente electronico']
+    collection = db['pacientes']
+
+    # buscar coincidencias usando los parámetros de búsqueda
+    resultados = collection.find({
+        'nombre': nombre,
+        'apellido_paterno': apellido_paterno,
+        'apellido_materno': apellido_materno
+    }, {
+        '_id': 0,
+        'generales.fecha_nacimiento': 1
+    })
+
+    # retornar los resultados de la búsqueda junto con la fecha de nacimiento
+    return [r for r in resultados]
+
+def check_ef(var):
+    if var == "":
+        var = 'sin alteraciones'
+    return var
+
+def note_show(consultas_previas, paciente, nota):
+    renglon = '\n'
+    evol = st.expander('CONSULTAS PREVIAS', expanded=True)
+    with evol:
+        # st.subheader(consultas_previas)
+        fechas_citas = []
+        for i in range(consultas_previas):
+            fechas_citas.insert(0,paciente[0]['consultas'][i]['fecha'])
+            # fechas_citas = sorted(fechas_citas, key=lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M"), reverse=True)
+        fecha_nota_prev = st.selectbox('Seleccione fecha de citas previas:', fechas_citas)
+        for consulta in paciente[0]["consultas"]:
+            if consulta["fecha"] == fecha_nota_prev:
+                if consulta['fecha'] == fechas_citas[-1]: # Se coteja si es la consulta de primera vez
+                    #Es consulta de primera vez
+                    st.subheader('Consulta de primera vez')
+                    # st.write(consulta)
+                    st.text_area('', nota, height=300)
+                else:
+                    # st.write("Se encontró una consulta en la fecha buscada:")
+                    # st.write(consulta)
+                    prev_cons = consulta
+
+                    consulta_anterior = ('##### '+prev_cons['fecha'] + renglon + renglon + #'- ' +
+                                            '> ' + prev_cons['presentacion'].replace('\n', ' ') + renglon + '- ' +
+                                            prev_cons['subjetivo'] + renglon + renglon +
+                                            '- '+'SOMATOMETRÍA Y SIGNOS VITALES:' + renglon +
+                                            'FC: ' + prev_cons['fc'] + ' lpm' + ' | ' +  'FR: ' + prev_cons['fr'] + ' rpm' + ' | ' + 'TA: ' + prev_cons['ta'] + ' mmHg' + ' | ' + ' ------- ' + 'PESO: ' +  str(prev_cons['peso']) + ' ' + 'kg' + '  ' + 'TALLA: ' + str(prev_cons['talla']) + ' ' + 'cm' + renglon + renglon + '- ' +
+                                            prev_cons['objetivo'] + renglon + renglon +
+                                            'PHQ-9: '+ prev_cons['clinimetrias']['phq9'] + ' ' + ' |   ' +
+                                            'GAD-7: '+ prev_cons['clinimetrias']['gad7'] + ' ' + ' |   ' +
+                                            'SADPERSONS: '+ prev_cons['clinimetrias']['sadpersons'] + ' ' + ' |   ' +
+                                            'YOUNG: '+ prev_cons['clinimetrias']['young'] + ' ' + ' |   ' +
+                                            'MDQ: '+ prev_cons['clinimetrias']['mdq'] + ' ' + ' |   ' +
+                                            'ASRS: '+ prev_cons['clinimetrias']['asrs'] + ' ' + ' |   ' +
+                                            'OTRAS: '+ prev_cons['clinimetrias']['otras_clini'] + ' ' + ' |   ' + renglon + renglon +
+                                            '##### '+ 'ANÁLISIS: ' + renglon +prev_cons['analisis'] + renglon + renglon +
+                                            '##### '+ 'PLAN: ' + renglon + prev_cons['plan'] + renglon + '--- '
+                                #  prev_cons['objetivo'] + renglon +
+                                #  prev_cons[''] + renglon +
+                                #  prev_cons[''] + renglon +
+                                #  prev_cons[''] + renglon +
+                                    ) #f'{consulta}{renglon}{renglon}MC: {mc}{renglon}PA: {pa}{renglon}{renglon}EXAMEN MENTAL{renglon}{renglon}{em}{renglon}{renglon}EXPLORACIÓN FÍSICA{renglon}{renglon}{somato_sv_merge}{renglon}{renglon}{ef_merge}{renglon}{alteraciones_merge}{renglon}{renglon}LABORATORIALES{renglon}- Previos: {labs_prev}{renglon}- Solicitados: {labs_nvos}{renglon}{renglon}DIAGNÓSTICO(S){renglon}{renglon}{dx}{renglon}{renglon}PRONÓSTICO: {pronostico}{renglon}{renglon}{clinimetria}{renglon}{renglon}ANÁLISIS{renglon}{renglon}{analisis}TRATAMIENTO{renglon}{renglon}{tx}'
+                    # st.subheader(f'Consulta subsecuente No: {consultas_previas}')
+                    # nota_revisada = st.text_area('', consulta_anterior, height=450)
+                    st.markdown(consulta_anterior)
+    return fechas_citas[-1]
+
+def last_note(consultas_previas, paciente, nota):
+    renglon = '\n'
+    fechas_citas = []
+    for i in range(consultas_previas):
+        fechas_citas.append(paciente[0]['consultas'][i]['fecha'])
+
+    return fechas_citas[-1], len(fechas_citas)
+
+
+def mongo_intial(mongodb_uri):
+    uri = mongodb_uri
+    client = MongoClient(uri)
+    db = client['expedinente_electronico'] #base de datos
+    pacientes = db['pacientes'] #colección
+    ensure_index('create',pacientes,'nombre_apellidos', [('nombres', 1), ('primer apellido', -1), ('segundo appelido', 1)])
+    return client, pacientes
+
+def mongo_connect(mongodb_uri):
+    uri = mongodb_uri
+    client = MongoClient(uri)
+    db = client['expedinente_electronico'] #base de datos
+    pacientes = db['pacientes'] #colección
+    ensure_index('create',pacientes,'nombre_apellidos', [('nombres', 1), ('primer apellido', -1), ('segundo appelido', 1)])
+    return client
+
+# def gdrive_up(local_file, final_name):
+#     g_login = GoogleAuth()
+#     g_login.LocalWebserverAuth()
+#     drive = GoogleDrive(g_login)
+#     file_name = local_file
+#     gfile = drive.CreateFile({'parents': [{'id': '1ESHu5ZblpwcCI5PrHP-80YrQ-NPiH7nm'}], 'title': final_name})
+#     # Read file and set it as the content of this instance.
+#     gfile.SetContentFile(file_name)
+#     gfile.Upload()
+#     print(file_name)
+#     # gfile.GetContentFile(file_name)
+#     print('---------DESPUES DE LEER ARCHIVO')
+#     file_url = 'https://drive.google.com/file/d/' + gfile['id'] + '/view'
+#     return file_url
+
+def gdrive_up(local_file, final_name):
+    gauth = GoogleAuth()
+    scope = ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/drive.file','https://www.googleapis.com/auth/drive.appdata']
+    # gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name('./service_account.json',scope)
+    gauth.service_account_json = 'service_account.json'
+    print(gauth)
+    drive = GoogleDrive(gauth)
+    file_name = local_file
+    gfile = drive.CreateFile({'parents': [{'id': '1ESHu5ZblpwcCI5PrHP-80YrQ-NPiH7nm'}], 'title': final_name})
+    # Read file and set it as the content of this instance.
+    gfile.SetContentFile(file_name)
+    gfile.Upload()
+    print(file_name)
+    # gfile.GetContentFile(file_name)
+    print('---------DESPUES DE LEER ARCHIVO')
+    file_url = 'https://drive.google.com/file/d/' + gfile['id'] + '/view'
+    return file_url
